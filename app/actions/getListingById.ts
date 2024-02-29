658f6d4d0db99364fb9e7f8c2b401f1b2a3706ff
thachthaiminh <gukucfvn@gmail.com>
2024-02-29 14:53:52 +0700

    Fix getListingById

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -1,4 +1,3 @@
-import { create } from "zustand";
 import prisma from "@/app/libs/prismadb";
 
 interface IParams {
@@ -8,22 +7,28 @@ interface IParams {
 export default async function getListingById(params: IParams) {
   try {
     const { listingId } = params;
+
     const listing = await prisma.listing.findUnique({
-      where: { id: listingId },
-      include: { user: true },
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
     });
 
     if (!listing) {
       return null;
     }
+
     return {
       ...listing,
-      createAt: listing.createdAt.toISOString(),
+      createdAt: listing.createdAt.toString(),
       user: {
         ...listing.user,
-        createdAt: listing.user.createdAt.toISOString(),
-        updatedAt: listing.user.updatedAt.toISOString(),
-        emailVerified: listing.user.emailVerified?.toISOString() || null,
+        createdAt: listing.user.createdAt.toString(),
+        updatedAt: listing.user.updatedAt.toString(),
+        emailVerified: listing.user.emailVerified?.toString() || null,
       },
     };
   } catch (error: any) {
--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
+
 import Heading from "../Heading";
 import HeartButton from "../HeartButton";
 
@@ -12,7 +13,7 @@ interface ListingHeadProps {
   locationValue: string;
   imageSrc: string;
   id: string;
-  currentUser: SafeUser | null;
+  currentUser?: SafeUser | null;
 }
 
 const ListingHead: React.FC<ListingHeadProps> = ({
@@ -23,21 +24,37 @@ const ListingHead: React.FC<ListingHeadProps> = ({
   currentUser,
 }) => {
   const { getByValue } = useCountries();
+
   const location = getByValue(locationValue);
+
   return (
     <>
       <Heading
         title={title}
         subtitle={`${location?.region}, ${location?.label}`}
       />
-      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+      <div
+        className="
+          w-full
+          h-[60vh]
+          overflow-hidden 
+          rounded-xl
+          relative
+        "
+      >
         <Image
-          alt="Image"
           src={imageSrc}
           fill
           className="object-cover w-full"
+          alt="Image"
         />
-        <div className="absolute top-5 right-5">
+        <div
+          className="
+            absolute
+            top-5
+            right-5
+          "
+        >
           <HeartButton listingId={id} currentUser={currentUser} />
         </div>
       </div>
